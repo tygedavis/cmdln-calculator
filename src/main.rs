0c32c272dc53bfdd28447453959097baacdbcdee
Tyge Davis <tyge.davis@gmail.com>
2025-08-28 18:05:51 -0600

    Add loop for happy path

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,27 @@ mod engine;
 use std::io;
 
 fn main() {
-    println!("Please type an input: ");
+    loop {
+        println!("Please type an input: ");
 
-    let mut user_input = String::new();
+        let mut user_input = String::new();
 
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("Cannot read input");
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Cannot read input");
 
-    let calculation = match engine::parse(&user_input) {
-        Ok(calc) => calc, // On success, get the Calculation struct out
-        Err(e) => {
-            // On failure, print the error and exit
-            println!("Parsing Error: {}", e);
-            return;
+        let calculation = match engine::parse(&user_input) {
+            Ok(calc) => calc, // On success, get the Calculation struct out
+            Err(e) => {
+                // On failure, print the error and exit
+                println!("Parsing Error: {}", e);
+                return;
+            }
+        };
+
+        match engine::evaluate(&calculation) {
+            Ok(num) => println!("Your answer is {}", num),
+            Err(e) => println!("There was an error: {}", e)
         }
-    };
-
-    match engine::evaluate(&calculation) {
-        Ok(num) => println!("Your answer is {}", num),
-        Err(e) => println!("There was an error: {}", e)
     }
 }
